68ffc85b7fec2395c4077dc8271fa781bf337f27
xqueue <37145530+xqueue@users.noreply.github.com>
2018-03-07 14:18:37 +0100

    Update AddressCheckResult.php

--- src/AddressCheck/AddressCheckResult.php
+++ src/AddressCheck/AddressCheckResult.php
@@ -6,7 +6,8 @@ use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use AddressCheckException;
+
+use XQueue\AddressCheck\AddressCheckException;
 
 class AddressCheckResult {
 	private $curlSession;
